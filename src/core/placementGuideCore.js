efc5fc6a77d86958d8ed10d42b77bc10c5fbd56c
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2018-08-13 15:03:49 +0300

    Fix corner coordinate detection

--- src/core/placementGuideCore.js
+++ src/core/placementGuideCore.js
@@ -1,4 +1,5 @@
-const path = require('path')
+const util = require('util')
+const _ = require('lodash')
 const BPromise = require('bluebird')
 const Jimp = require('jimp')
 
@@ -31,28 +32,39 @@ const isWhiteOrTransparent = (r, g, b, a) => {
   return isWhite || isTransparent
 }
 
-function findCornerPixel(pixels, predicate) {
-  let currentPixel = pixels[0]
-  for (let i = 0; i < pixels.length; ++i) {
-    const pixel = pixels[i]
-    if (predicate(currentPixel, pixel)) {
-      currentPixel = pixel
-    }
+function sortCounterClockwise(topLeft, pixels) {
+  const centerX = _.sumBy(pixels, p => p.x) / pixels.length
+  const centerY = _.sumBy(pixels, p => p.y) / pixels.length
+
+  // Sort based on the angle from center coordinate
+  // https://stackoverflow.com/questions/1709283/how-can-i-sort-a-coordinate-list-for-a-rectangle-counterclockwise
+  const sorted = _.sortBy(pixels, (p) => {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/atan2#Description
+    const normalizedAngle = (Math.atan2(p.x - centerX, p.y - centerY) + (2 * Math.PI)) % (2 * Math.PI)
+    return normalizedAngle
+  })
+
+  const index = _.findIndex(sorted, p => p.x === topLeft.x && p.y === topLeft.y)
+  if (index === -1) {
+    throw new Error(`topLeft coordinate not found from pixels: ${util.inspect(topLeft)} ${util.inspect(pixels)}`)
   }
-  return currentPixel
+
+  return {
+    topLeft: sorted[index],
+    bottomLeft: sorted[(index + 1) % sorted.length],
+    bottomRight: sorted[(index + 2) % sorted.length],
+    topRight: sorted[(index + 3) % sorted.length],
+  }
 }
 
 function findCorners(pixels) {
-  const topLeft = findCornerPixel(pixels, (cP, p) => p.x + p.y < cP.x + cP.y)
-  const topRight = findCornerPixel(pixels, (cP, p) => -p.x + p.y < -cP.x + cP.y)
-  const bottomRight = findCornerPixel(pixels, (cP, p) => p.x + p.y > cP.x + cP.y)
-  const bottomLeft = findCornerPixel(pixels, (cP, p) => p.x - p.y < cP.x - cP.y)
-  return {
-    topLeft,
-    topRight,
-    bottomRight,
-    bottomLeft,
-  }
+  const minX = _.minBy(pixels, p => p.x)
+  const maxY = _.maxBy(pixels, p => p.y)
+  const maxX = _.maxBy(pixels, p => p.x)
+  const minY = _.minBy(pixels, p => p.y)
+  const topLeft = _.minBy(pixels, p => p.x + p.y)
+
+  return sortCounterClockwise(topLeft, [minX, maxY, maxX, minY])
 }
 
 async function getPlacementData(image) {
--- src/middleware/injectRequestId.js
+++ src/middleware/injectRequestId.js
@@ -1,7 +1,7 @@
 const uuidv4 = require('uuid/v4')
 
 const createInjectRequestId = () => (req, res, next) => {
-  req.id = req.headers['x-request-id'] || req.headers['x-amzn-trace-id'] || `aito-${uuidv4()}`
+  req.id = req.headers['x-request-id'] || req.headers['x-amzn-trace-id'] || `express-${uuidv4()}`
   next()
 }
 
